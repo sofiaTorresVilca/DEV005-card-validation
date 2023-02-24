8f54ae5d531a4988ab99aa69876a04b9cf3b4842
sofiatorresvilca <sofiauldemia@gmail.com>
2023-02-24 09:44:24 -0500

    mi repositorio

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import validator from './validator.js';
+// Declarando  mis variables
+const nombre = document.getElementById('nombre');
+const cardNumber = document.getElementById('cardNumber');
+const btn = document.getElementById('btn');
+const texto = document.getElementById('texto');
+const texto2 = document.getElementById('texto2');
+const imagen = document.getElementById('imagen');
+const mascaraContent = document.getElementById('contenedorMascara');
+const agregar = document.getElementById('textoMascara');
 
-console.log(validator);
+
+// Declarando mi evento
+btn.addEventListener('click', validate);
+
+
+//Declarando mi funcion y sus condiciones
+function validate() {
+  const valorNombre = nombre.value;
+  const valorNumero = cardNumber.value;
+  const luhm = validator.isValid(valorNumero);
+  const mostrarMascara  = validator.maskify(valorNumero);
+
+
+  //Condicion para el input del nombre
+  if (valorNombre === '') {   
+    texto2.innerHTML = ' Este campo es obligatorio'
+    nombre.style.border = '1px solid red'       
+      
+  } else {
+    texto2.innerHTML = '';
+    nombre.style.border = '1px solid green'  
+  }
+
+
+  //Condicion para el input de la tarjeta
+  if (valorNumero === '') {
+    texto.innerHTML = ' Este campo es obligatorio'
+    cardNumber.style.border = '1px solid red'
+      
+  }else if(luhm) { //Aqui validamos si cumple el algoritmo 
+    texto.innerHTML = 'Tarjeta válida'
+    cardNumber.style.border = '1px solid green'
+
+
+  }else if (luhm === false && valorNumero.length < 5 ) {
+    texto.innerHTML = ' Error de validacion'
+    cardNumber.style.border = '1px solid red'
+    agregar.innerHTML = valorNumero;
+
+  } else {
+    texto.innerHTML = ' Error de validacion'
+    cardNumber.style.border = '1px solid red'  
+  }
+ 
+  if (valorNombre !=='' && valorNumero !== '') {
+    mostrarMascara;
+    agregar.innerHTML = mostrarMascara;
+    imagen.style.display = 'none';
+    mascaraContent.style.display = 'grid';
+  } 
+}
+
+
+// console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,54 @@
 const validator = {
-  // ...
+  isValid:function(valorNumero) {
+
+    const newArray = valorNumero.split('').reverse().map(x => parseInt(x));
+   
+    for (let i = 0; i < newArray.length; i++) {
+
+      if (i%2 !== 0) {     //encontrar las pocisiones pares según el algoritmo.
+        newArray[i]*=2 
+       
+        if (newArray[i] > 9)  {
+          newArray[i] -= 9;
+  
+        } else {
+          newArray[i]*= 1;
+        }
+
+      } else {
+        newArray[i]*=1;
+      }
+    }
+
+    let total = 0;
+    //recorrer totdos los numero actualizados
+    for (let i = 0; i < newArray.length; i++) {
+      total = total + newArray[i];
+    }
+
+
+    if (total % 10 === 0) {
+      return true;
+      
+    } else  {
+      return false;
+      
+    }
+
+  },
+
+  maskify:function(valorNumero) {
+  
+    let nuevoNumero = valorNumero;
+    if (valorNumero.length > 4) {    
+      const numerosSimbolo = '#'.repeat(valorNumero.length -4);    
+      const mostrarUltimos = valorNumero.substring(valorNumero.length-4);
+      nuevoNumero = numerosSimbolo + mostrarUltimos;
+    } 
+    return nuevoNumero;
+   
+  }
+
 };
 
 export default validator;
